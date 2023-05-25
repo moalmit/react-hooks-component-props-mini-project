1dd81d647a695d9fb876b1005c051998db0b8e27
Mohammed Al Mitwalli <mohammed.almitwalli@gmail.com>
2023-05-25 16:21:47 -0700

    Complete assignment

---
+++ src/components/Article.js
@@ -0,0 +1,16 @@
+import React from "react"
+import blogData from "../data/blog"
+
+
+function Article ({title, date, preview}) {
+    return (
+        <article>
+            <h3>{title}</h3>
+            <small>{date ? date : "January 1, 1970"}</small>
+            <p>{preview}</p>
+        </article>
+
+    )
+}
+
+export default Article
